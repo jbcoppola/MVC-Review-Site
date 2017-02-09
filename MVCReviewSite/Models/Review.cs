5bb09df2c882916a06f601a84a2d727d7cfb8e83
Joseph Coppola <jbcoppola@gmail.com>
2017-02-09 15:57:20 -0500

    Initial commit.

---
+++ MVCReviewSite/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSite.Models
+{
+	public class Review
+	{
+		[Key]
+		public int ID { get; set; }
+
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public DateTime PublishDate { get; set; }
+		public int Rating { get; set; }
+
+		[ForeignKey]
+		public string Category { get; set; }
+	}
+}
